3efabcd27fbc6fa8586b90f5728dfce195a47900
TienMinh25 <letienminh2512@gmail.com>
2024-02-23 18:42:23 +0700

    Add unit test for authorization to rep

---
+++ back_end/src/tests/middleware/authorization.test.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+const authorization = require("../../middleware/authorization");
+
+jest.mock("jsonwebtoken", () => ({
+  verify: jest.fn(),
+}));
+
+describe("authorization middleware", () => {
+  let req, res, next;
+
+  beforeEach(() => {
+    process.env.SECRET_KEY = "test@1234";
+    req = {
+      cookies: {
+        authCookie: "tokenJWT",
+      },
+    };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+    };
+
+    next = jest.fn().mockReturnThis();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should call next() if token is verified", async () => {
+    jwt.verify.mockReturnValue({ username: "johndoe" });
+
+    await authorization(req, res, next);
+
+    expect(jwt.verify).toHaveBeenCalledWith("tokenJWT", process.env.SECRET_KEY);
+    expect(next).toHaveBeenCalled();
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+  });
+
+  it("should return 401 if token verification fails", async () => {
+    jwt.verify.mockImplementation(() => {
+      throw new Error("Token is unvalid");
+    });
+
+    await authorization(req, res, next);
+
+    expect(jwt.verify).toHaveBeenCalledWith("tokenJWT", process.env.SECRET_KEY);
+    expect(next).not.toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: "Unauthorized request!" });
+  });
+});
